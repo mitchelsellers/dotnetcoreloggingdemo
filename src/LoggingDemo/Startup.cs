83f702299cc665e370951d3639f10e158a841198
Mitchel Sellers <msellers@iowacomputergurus.com>
2021-01-29 11:32:54 -0600

    Updated presentation and added correlation id

--- src/LoggingDemo/Startup.cs
+++ src/LoggingDemo/Startup.cs
@@ -44,6 +44,7 @@ namespace LoggingDemo
                 .AddEntityFrameworkStores<ApplicationDbContext>();
 
             services.AddControllersWithViews();
+            services.AddHttpContextAccessor();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
